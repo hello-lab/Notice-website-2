cdb44e53cec32e9591eda632da36d50a442a7d29
EZIO AUDITORE <ezioauditoredefinerz@gmail.com>
2024-08-24 11:33:45 +0530

    first committt

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 setInterval(() => {
-    document.getElementById('clock').innerHTML = Date().split(' ')[4];
+    document.getElementById('clock').innerHTML ='<img style=" width:5vh;" src="assets/pin.png">' +Date().split(' ')[4]+'<img style=" width:5vh;" src="assets/pin.png">';
 }, 1000);
 
 function hey() {
-    document.getElementById('date').innerHTML = Date().split(' ')[2] + ' ' + Date().split(' ')[1] + ' ' + Date().split(' ')[3];
+    document.getElementById('date').innerHTML ='<img style=" width:5vh;" src="assets/pin.png">' + Date().split(' ')[2] + ' ' + Date().split(' ')[1] + ' ' + Date().split(' ')[3]+'<img style=" width:5vh; "src="assets/pin.png">' ;
 }
